bf7a968de36cad7cf76a3364fd1e5d73f7e8b79b
mbarn <mbarney@ix.netcom.com>
2021-02-10 06:56:43 -0500

    first commit

---
+++ javaClass/Lab-Datatypes/src/VariableDeclaration.java
@@ -0,0 +1,38 @@
+
+public class VariableDeclaration {
+
+	public static void main(String[] args) {
+    /*    boolean on = true;
+        System.out.println(on);
+        
+        short s = 16;
+        System.out.println(s);
+
+        int i = -32;
+        System.out.println(i);
+
+        float f = 3839.34839f;
+        System.out.println(f);
+        
+        long l = 4294967296l;
+	    System.out.println(l);
+	    
+	    char c = 'a';
+	    System.out.println(c); */
+	
+		// Casting
+		long a = 10393L;
+		int i = (int)a;
+	    System.out.println(i);
+
+	       int w = 200;
+           //cast to a short
+	       
+	       short s = (short)w;
+	       System.out.println(s);
+
+
+
+	}
+
+}
